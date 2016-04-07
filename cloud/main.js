bf013db5c8bdefd92f4526bff36ee1ed0f9c10e3
Irwan Nugraha <patrick.jtk@gmail.com>
2016-04-07 18:09:22 +0700

    omset

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,17 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('omset_report', function(req, res){
+	var query = new Parse.Query("Transaksi");
+	query.equalTo("objectId", "DZzREmkddM");
+	query.find({
+		success: function(results) {
+		  
+		  response.success(results[i]);
+		},
+		error: function() {
+		  response.error("movie lookup failed");
+		}
+	});
+});
