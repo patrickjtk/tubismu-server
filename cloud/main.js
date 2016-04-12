b67a5a18d262dc813da75b5733a3c498ed743eda
Irwan Nugraha <patrick.jtk@gmail.com>
2016-04-12 15:38:22 +0700

    init dev

--- cloud/main.js
+++ cloud/main.js
@@ -7,11 +7,52 @@ Parse.Cloud.define('omset_report_monthly', function(req, res){
 	var query = new Parse.Query("Transaksi");
 	console.log(req.params.startMonth);
 	console.log(req.params.endMonth);
-	query.equalTo("objectId", "DZzREmkddM");
+	query.greaterThanOrEqualTo("createdAt", new Date(req.params.startMonth));
+	var endMonth = new Date(endMonth);
+	endMonth.setMonth(endMonth.getMonth() + 1);
+	query.lessThanOrEqualTo("createdAt", endMonth);
+	
+  	query.find({
+	  success: function(results) {
+	  	console.log(results);
+	    // alert("Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	    
+
+	    console.log(dat);
+	    
+	  },
+	  error: function(error) {
+	    alert("Error: " + error.code + " " + error.message);
+	  }
+	});
+
 	query.find({
 		success: function(results) {
-		  
-		  res.success(req.params);
+			var dat = [];
+			if(results.length > 0){
+		    	var date =  results[0].attributes.createdAt.getMonth();
+			    var count = 0;
+			    for (var i = 0; i < results.length; i++) {
+			    	var object = results[i];
+					if(date != object.attributes.createdAt.getMonth()){
+						console.log("beda");
+						dat.push({date: date, count: count});
+						date =  results[i].attributes.createdAt.getMonth();
+						count = 0;
+					}
+					console.log(object.attributes.produk.length);
+				    for (var j = 0; j < object.attributes.produk.length; j++) {
+			      		count  = count + object.attributes.produk[j].harga;
+			      	};
+			      	if(i+1 == results.length){
+			      		dat.push({date: date, count: count});
+			      	}
+			      // alert(object.id + ' - ' + object.get('playerName'));
+			      
+			    }
+		    }
+		  	res.success(dat);
 		},
 		error: function(error) {
 		  res.error(error.message);
