1cf0d9c33d56c9ede17d94854cbad8f3bc4f2475
Irwan Nugraha <patrick.jtk@gmail.com>
2016-04-12 13:41:28 +0700

    test;

--- cloud/main.js
+++ cloud/main.js
@@ -3,8 +3,10 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define('omset_report', function(req, res){
+Parse.Cloud.define('omset_report_monthly', function(req, res){
 	var query = new Parse.Query("Transaksi");
+	console.log(req.param.startMonth);
+	console.log(req.param.endMonth);
 	query.equalTo("objectId", "DZzREmkddM");
 	query.find({
 		success: function(results) {
