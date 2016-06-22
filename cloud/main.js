d564d2a503cdc64a9d93295e715ac6906b4fdad6
Irwan Nugraha <patrick.jtk@gmail.com>
2016-06-22 17:03:52 +0700

    report produk

--- cloud/main.js
+++ cloud/main.js
@@ -4,17 +4,86 @@ Parse.Cloud.define('hello', function(req, res) {
 });
 
 Parse.Cloud.define('omset_report_monthly', function(req, res){
-	var query = new Parse.Query("Transaksi");
+	var query = new Parse.Query("LamasysTransaksi");
 	console.log(req.params.startMonth);
 	console.log(req.params.endMonth);
-	query.equalTo("objectId", "DZzREmkddM");
+	query.greaterThanOrEqualTo("createdAt", new Date(req.params.startMonth));
+	var endMonth = new Date(req.params.endMonth);
+	endMonth.setMonth(endMonth.getMonth() + 1);
+	query.lessThanOrEqualTo("createdAt", endMonth);
+
 	query.find({
 		success: function(results) {
-		  
-		  res.success(req.params);
+			var dat = [];
+			if(results.length > 0){
+		    	var date =  results[0].attributes.tanggalTrx.getMonth();
+			    var count = 0;
+			    for (var i = 0; i < results.length; i++) {
+			    	var object = results[i];
+					if(date != object.attributes.tanggalTrx.getMonth()){
+						console.log("push");
+						dat.push({date: date, count: count});
+						date =  results[i].attributes.tanggalTrx.getMonth();
+						count = 0;
+					}
+					console.log(object.attributes.produk.length);
+				    for (var j = 0; j < object.attributes.produk.length; j++) {
+			      		count  = count + object.attributes.produk[j].harga;
+			      	};
+			      	if(i+1 == results.length){
+			      		console.log("push end");
+			      		dat.push({date: date, count: count});
+			      	}
+			      // alert(object.id + ' - ' + object.get('playerName'));
+			      
+			    }
+		    }
+		  	res.success(dat);
 		},
 		error: function(error) {
 		  res.error(error.message);
 		}
 	});
 });
+
+Parse.Cloud.define('produk_report_monthly', function(req, res){
+	var query = new Parse.Query("LamasysTransaksiDetil");
+	console.log(req.params.startMonth);
+	console.log(req.params.endMonth);
+	query.greaterThanOrEqualTo("createdAt", new Date(req.params.startMonth));
+	var endMonth = new Date(req.params.endMonth);
+	endMonth.setMonth(endMonth.getMonth() + 1);
+	query.lessThanOrEqualTo("createdAt", endMonth);
+	query.include("Produk");
+	query.orderByAscending("produkName");
+	query.find({
+		success: function(results) {
+
+			if(results.length > 0){
+				var dat = [];
+				var count = 0;
+				var produk = results[0].get("Produk").get("namaProduk");
+				for (var i = 0; i < results.length; i++) {
+					var object = results[i];
+					
+					if(produk != object.get("Produk").get("namaProduk")){
+						console.log("push");
+						dat.push({date: date, count: count});
+						produk =  results[i].get("Produk").get("namaProduk");
+						count = 0;
+					} else{
+						count = count + 1;
+					}
+					if(i+1 == results.length){
+			      		console.log("push end");
+			      		dat.push({produkName: produk, qty: count});
+			      	}
+				}
+			}
+			res.success(dat);
+		},
+		error: function(error) {
+		  res.error(error.message);
+		}
+	});
+})
